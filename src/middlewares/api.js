eefbca7e438f5350f6e29dccb27379c5e3d7b6a0
leonh <hello@leonh.fr>
2018-10-23 18:30:09 +0200

    fix: changed port

--- src/middlewares/api.js
+++ src/middlewares/api.js
@@ -1,16 +1,4 @@
-// ACTION EXAMPLE
-//
-// const action = {
-//   type: '...'
-//   api: {
-//     endpoint:,
-//     body:,
-//     headers:,
-//     …
-//   }
-// }
-
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'http://localhost:4000';
 
 export default store => next => action => {
   if (!action.api) return next(action);
